e9da399b140d66ec4f64fd99fa83edb933aa8c79
Pablo Olivares <pablolivares1502@gmail.com>
2024-04-01 14:42:52 +0200

    Validation and training losses plotted together advances #6
    Graphics will accept both training and validation losses. If valiadation losses available, they will both be printed in the graphic.

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,12 +1,21 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_loss(epoch_losses, plot_path):
+def plot_loss(training_epoch_losses, validation_epoch_losses, plot_path):
     plt.figure(figsize=(10, 5))
-    plt.plot(np.arange(1, len(epoch_losses) + 1), epoch_losses, label="Training Loss")
-    plt.title("Training Loss Over Epochs")
+    
+    epochs = np.arange(1, len(training_epoch_losses) + 1)
+    
+    plt.plot(epochs, training_epoch_losses, label="Training Loss", marker='o')
+    
+    if validation_epoch_losses:
+        validation_epochs = np.linspace(1, len(training_epoch_losses), num=len(validation_epoch_losses))
+        plt.plot(validation_epochs, validation_epoch_losses, label="Validation Loss", marker='x')
+    
+    plt.title("Loss Over Epochs")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
     plt.legend()
+    plt.grid(True)
     plt.savefig(plot_path)
     plt.close()
